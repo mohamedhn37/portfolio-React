273cb750e79965bbff202fb9ecbca04779a1d41c
Mohamed Hannaoui <hannaoui20.mohamed@gmail.com>
2023-04-06 13:29:41 +0000

    darkModeModification

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
-import React from "react";
-import {Routes, Route} from 'react-router-dom'
-import {DarkMode, GlobaleStyles, LightTheme, DarkTheme} from "./DarkMode";
-import styled,  {ThemeProvider} from "styled-components";
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import {
+  DarkMode,
+  GlobaleStyles,
+  LightTheme,
+  DarkTheme,
+} from "./DarkMode";
+import styled, { ThemeProvider } from "styled-components";
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import Home from "./Pages/Home/Home";
-import Skills from "./Pages/Skills/Skills"
-import Service from "./Pages/Service/Service"
+import Skills from "./Pages/Skills/Skills";
+import Service from "./Pages/Service/Service";
 import Portfolio from "./Pages/Portfolio/Portfolio";
 import Contact from "./Pages/Contact/Contact";
 
+const  App = () => {
+  
+  const [theme, setTheme] = useState(
+    () => localStorage.getItem("theme") || "light"
+  );
 
-function App() {
-  const [theme, toggleTheme] = DarkMode()
-  const themeMode = theme === 'light' ? LightTheme : DarkTheme
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+  };
+  const themeMode = theme === "light" ? LightTheme : DarkTheme;
 
   return (
-    <> 
+    <>
       <ThemeProvider theme={themeMode}>
-        <GlobaleStyles/>
-          <Header theme={theme} toggleTheme={toggleTheme}/>
-          <Routes>
-              <Route path="/" element={<Home/>}/>
-              <Route path="/Skills" element={<Skills/>}/>
-              <Route path="/Service" element={<Service/>}/>
-              <Route path="/Portfolio" element={<Portfolio/>}/>
-              <Route path="/Contact" element={<Contact/>}/>
-          </Routes>   
-          <Footer/>
+        <GlobaleStyles />
+        <Header theme={theme} toggleTheme={toggleTheme} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Skills" element={<Skills />} />
+          <Route path="/Service" element={<Service />} />
+          <Route path="/Portfolio" element={<Portfolio />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+        <Footer />
       </ThemeProvider>
     </>
   );
