e132fa9dda5f44d4ab5bf3c2a6ce5ca295bfce33
Mohamed Hannaoui <hannaoui20.mohamed@gmail.com>
2023-05-08 12:55:40 +0100

    textmodification

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
           <div className="col-md-7 mb-3 home">
             <h1 className="homeTitle">I BUILD BEAUTIFUL MODERN WEBSITES</h1>
             <p className="homeSubstitle">I'm Mohamed HANNAOUI | Full-Stack Developer</p>
-            <p className='homeShortDescription'>I BUILD BEAUTIFUL MODERN WEBSITES I'm Mohamed HANNAOUI | Full-Stack Developer As a freelance Front-end developer, I've found that many clients enjoy my unique design style, high quality of work, and reasonable rates.</p>
+            <p className='homeShortDescription'>I BUILD BEAUTIFUL MODERN WEBSITES I'm Mohamed HANNAOUI | Front-end developer, I've found that many clients enjoy my unique design style, high quality of work, and reasonable rates.</p>
             <a href="/Contact" className="btn btn-danger rounded-pill px-5 py-2 text-white fw-bold">CONTACT ME</a>
           </div>
           <div className="col-md-5 text-center bg-light p-4 rounded-3">
@@ -25,8 +25,8 @@ const Home = () => {
                 <span className='avilable'></span>
             </figure>
             <h3 className='text-center py-2 fw-bold'>MOHAMED HANNAOUI</h3>
-            <p className='text-center py-1 text-danger fw-bold'>FULL-STACK DEVELOPER</p>
-            <h5>I am a full-time freelance full-stack developer from Morocco. who specializes in creating dynamic and beautiful web pages. I have been in the field for nearly 2 years and have been loving every minute of it.</h5>        
+            <p className='text-center py-1 text-danger fw-bold'>FRONT-END DEVELOPER</p>
+            <h5>Mohamed HANNAOUI, 22 ,I am a beginner frontend developer (with knowledge in backend), passionate about creating websites and applications. Having recently obtained my basic degree in computer science, database option, I am now ready to apply my knowledge to create innovative solutions. I enjoy working on challenging projects and finding creative solutions to development problems.</h5>        
             <button onClick={openCV} className="btn btn-danger rounded-pill px-5 py-2 text-white fw-bold my-3">Download My Resume</button>
           </div>
         </div>
